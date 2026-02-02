44e9b8c59269b94f91db600155f2c437f3f69380
siphenumerouno-hash <siphenumerouno@gmail.com>
2026-02-02 13:31:56 +0200

    chore: capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.siphe.metrogym',
+  appName: 'MetroGym',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
+ 
